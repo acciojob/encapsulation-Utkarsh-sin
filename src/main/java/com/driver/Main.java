3edd92cde77ea92f6144a4c3519b8cd2cdc1db21
Utkarsh-sin <sinhautkarsh38@gmail.com>
2023-01-16 02:57:14 +0530

    Encap

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,14 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+        //System.out.println(obj.name="Utkarsh");
+        //name has private access in com.driver.RWOnly
+
+        obj.setName("Utkarsh");
+        obj.getName();
+
+
+    }
 }
